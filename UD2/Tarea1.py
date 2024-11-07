ca2712afd8b832b47f11dc71e286f3f06bd369a5
Ángel MB <114161450+Angelmbx@users.noreply.github.com>
2024-11-07 10:58:32 +0100

    Borrado Tarea_01 fuera de carpeta Tareas

--- UD2/Tarea1.py
+++
@@ -1,7 +0,0 @@
-### Escribir un programa que almacene a cadea Ola Mundo! nunha variable e logo mostre por pantalla o contido da variable. ###
-
-if __name__ = __main__ :
-    
-    saudo = input()
-
-    print(saudo)
