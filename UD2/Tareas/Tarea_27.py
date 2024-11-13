40a549b822ed9048bc7149ec214851ab953ab91c
Angelmbx <mirandabl.angel@gmail.com>
2024-11-13 20:21:44 +0100

    Tarea 27 corregida

--- UD2/Tareas/Tarea_27.py
+++ UD2/Tareas/Tarea_27.py
@@ -2,20 +2,23 @@
 # A función debe recibir a cantidade sen IVE e a porcentaxe de IVE a aplicar, e devolver o total da factura. 
 # Se se invoca a función sen pasarlle a porcentaxe de IVE, deberá aplicar un 21%. ##
 
-if __name__ == "__main__":
+if __name__ == "__main__":      
 
     factura_sen_ive = float(input('Introduza o importe da súa factura sen IVE: '))
-    tipo_ive = float(input('Indique a porcentaxe de IVE a aplicar: 21, 10, ou 4'))/100 +1
-    IVE_XERAL = 1.21
-    factura_con_ive = 0
+    tipo_ive = input('Indique a porcentaxe de IVE a aplicar (4, 10 ou deixar baleiro para o xeral 21%): ')
 
-    if tipo_ive != null:
-        def ive_calculator(tipo_ive):
-            factura_con_ive = factura_sen_ive * tipo_ive
-            return factura_con_ive
+    if tipo_ive == "":
+        tipo_ive = 21
     else:
-        print('Tipo de IVE non escificado, aplicarase o IVE xeral')
-        factura_con_ive = factura_sen_ive * IVE_XERAL
-
+        tipo_ive = float(tipo_ive)
     
-    print('A súa factura aplícandolle o')
+    if tipo_ive not in [4, 10, 21]:
+        print('IVE non válido. Aplicarase o IVE xeral (21%)...')
+        tipo_ive = 21
+    
+    def factura_con_ive_calculator(factura_sen_ive, tipo_ive=21):
+        return factura_sen_ive * (1 + tipo_ive / 100)
+
+    total_factura = factura_con_ive_calculator(factura_sen_ive, tipo_ive)
+    
+    print(f'O montante da súa factura é de {total_factura:.2f}')
