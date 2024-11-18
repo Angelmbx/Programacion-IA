a1fa2e9db95df096973b5db8b786026b05e30894
Angelmbx <mirandabl.angel@gmail.com>
2024-11-18 09:46:17 +0100

    Teoría pytest

---
+++ UD2/Teoria clase/probatest/src/probatest/main.py
@@ -0,0 +1,12 @@
+
+def suma(n1,n2):
+    return n1+n2
+
+
+if __name__ == "__main__":
+    
+    numero_1 = input('Dame un numero')
+    numero_2 = input ('Dame outro numero')
+
+    result = suma(numero_1, numero_2)
+    print(f'O resultado é {result}')
---
+++ UD2/Teoria clase/probatest/src/test/test_proba.py
@@ -0,0 +1,11 @@
+from probatest.main import suma
+
+
+def test_1mas0():
+    assert suma(1,0) == 1
+    assert suma(0,1) == 1
+    # si todos los assert estan bien dentro de la misma funcion devuelve True, si uno falla devuelve False
+
+def test_outro():
+    assert suma(5,4) == 9
+    assert suma(3,45) == 1
