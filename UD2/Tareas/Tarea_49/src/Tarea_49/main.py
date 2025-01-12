4c1f1e0f10660517bce682f5599dab08381cf35e
Angelmbx <mirandabl.angel@gmail.com>
2025-01-12 01:23:53 +0100

    Ejercicio resuelto, faltan test

--- UD2/Tareas/Tarea_49/src/Tarea_49/main.py
+++ UD2/Tareas/Tarea_49/src/Tarea_49/main.py
@@ -1,44 +1,19 @@
 ## No sistema temos presente usuarios. Cada un deles ten un nome, un nome de usuario, un contrasinal e un número de tarxeta.
 #  O número de tarxeta segue a formula de Luhn. Os usuarios poden ter establecido o número de tarxeta ou non, en todo caso, 
 #  se o teñen ten que seguir a formula. Pensa primeiramente que relación existe entre os usuarios e os números de Lunh, e logo implementa. ##
-def luhn_method(numero):
 
-        ## VALIDACIÓN DE NÚMERO: Falta quitar espacios y unicos caracteres validos [0..9]       
-        if len(numero) < 2:
-            return False 
-            
-        
-        numero = numero[::-1]
-        pares = []
+from errores import error_de_formato, error_numero_caracteres, error_algoritmo_luhn
+from usuario import Usuario
 
-        # Meto los elementos en la posicion par y los multiplico *2
-        for index in range(0, len(numero)):
-            if not index % 2 == 0 :
-                elemento =  int(numero[index])*2
-                if elemento > 9:
-                    pares.append(elemento -9)
-                else:
-                    pares.append(elemento)
-
-                
-        
-
-         ## Comprobación método luhn
-        if sum(pares) % 10 == 0:
-            return True
-        else:
-            return False
 
 if __name__ == "__main__":
-    class usuario:
-        def init(self, nome, username, password, tarxeta = None):
-            self.nome = nome
-            self.username = username
-            self.password = password
-            if luhn_method(tarxeta):
-                self.tarxeta = tarxeta
-            else:
-                raise Exception
-        
-
-    print(luhn_method("4539319503436467")) # Da False, debería dar True
+    
+    try:
+        usuario_1 = Usuario(nombre="Paco", username="magic_paco", password = "123.", tarjeta= "4539 3195 0343 6467" )
+        print(usuario_1.mostrar_usuario())
+    except error_numero_caracteres as invalid_length:
+        print(invalid_length)
+    except error_de_formato as format_error:
+        print(format_error)
+    except error_algoritmo_luhn as invalid_card_number:
+        print(invalid_card_number)
---
+++ UD2/Tareas/Tarea_49/src/Tarea_49/usuario.py
@@ -0,0 +1,44 @@
+from errores import error_algoritmo_luhn, error_de_formato, error_numero_caracteres
+
+
+def algoritmo_luhn(numero):
+
+    # Eliminar espacios
+    numero = numero.replace(" ", "")  
+
+    if len(numero) < 2:
+        raise error_numero_caracteres
+    if not numero.isdigit():
+        raise error_de_formato
+    
+    # Invertir número
+    numero = numero[::-1]
+    suma = 0  
+
+    for index in range(len(numero)):
+        digito = int(numero[index]) # convierto cada numero de la cadena a entero
+        
+        if index % 2 == 1:  # Como se empieza a contar en 0, se multiplican * 2 las posiciones cuyos números sean impares (1 es la 2ª posicion, 3 la 4ª, etc.)
+            digito *= 2
+            if digito > 9: 
+                digito -= 9
+
+        suma += digito
+
+   
+    return suma % 10 == 0
+
+
+
+class Usuario:
+    def __init__(self, nombre, username, password, tarjeta = None):
+        self.nombre = nombre
+        self.username = username
+        self.password = password
+        if algoritmo_luhn(tarjeta):
+            self.tarjeta = tarjeta
+        else:
+            raise error_algoritmo_luhn(tarjeta)
+            
+    def mostrar_usuario(self):
+        return f"Nombre: {self.nombre}, Username: {self.username}, Password: {self.password}, Tarjeta: {self.tarjeta}"
