2a63c22ecb95d598f4b83a9139c3586c1a79b5d4
Ángel MB <114161450+Angelmbx@users.noreply.github.com>
2025-01-12 01:27:04 +0100

    Aclaración comentario en usuario.py

--- UD2/Tareas/Tarea_49/src/Tarea_49/usuario.py
+++ UD2/Tareas/Tarea_49/src/Tarea_49/usuario.py
@@ -18,7 +18,7 @@ def algoritmo_luhn(numero):
     for index in range(len(numero)):
         digito = int(numero[index]) # convierto cada numero de la cadena a entero
         
-        if index % 2 == 1:  # Como se empieza a contar en 0, se multiplican * 2 las posiciones cuyos números sean impares (1 es la 2ª posicion, 3 la 4ª, etc.)
+        if index % 2 == 1:  # Como se empieza a contar en 0, se multiplican * 2 las posiciones cuyo índice sea un número impare (índice 1 es la 2ª posicion, índice 3 la 4ª, etc.)
             digito *= 2
             if digito > 9: 
                 digito -= 9
