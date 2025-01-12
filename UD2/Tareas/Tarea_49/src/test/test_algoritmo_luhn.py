db18fed2b91a78bbcfc2241e9ca170781a7a2646
Angelmbx <mirandabl.angel@gmail.com>
2025-01-12 02:02:19 +0100

    test añadido

---
+++ UD2/Tareas/Tarea_49/src/test/test_algoritmo_luhn.py
@@ -0,0 +1,35 @@
+import pytest
+from Tarea_49.usuario import Usuario
+from Tarea_49.algoritmo_luhn import algoritmo_luhn
+from Tarea_49.errores import error_de_formato, error_numero_caracteres, error_algoritmo_luhn 
+
+def test_algoritmo_luhn():
+    # Tarjeta valida
+    assert algoritmo_luhn("4539319503436467") == True
+    assert algoritmo_luhn("6011514433546201") == True
+    
+    # Tarjeta inválida
+    assert algoritmo_luhn("1234567812345670") == False
+    assert algoritmo_luhn("1234567812345678") == False
+
+
+def test_usuario_creacion_valida():
+    
+    usuario = Usuario(nombre="Paco", username="magic_paco", password="123.", tarjeta="4539319503436467")
+    assert usuario.nombre == "Paco"
+    assert usuario.username == "magic_paco"
+    assert usuario.password == "123."
+    assert usuario.tarjeta == "4539319503436467"  
+
+def test_usuario_tarjeta_invalida():
+    with pytest.raises(error_algoritmo_luhn):
+        Usuario(nombre="Paco", username="magic_paco", password="123.", tarjeta="1234567812345678")
+
+def test_usuario_formato_tarjeta_invalido():
+    with pytest.raises(error_de_formato):
+        Usuario(nombre="Paco", username="magic_paco", password="123.", tarjeta="Tarjeta58")
+
+def test_usuario_caracteres_insuficientes():
+    # Caso donde el número de caracteres de la tarjeta es insuficiente
+    with pytest.raises(error_numero_caracteres):
+        Usuario(nombre="Paco", username="magic_paco", password="123.", tarjeta="3")
