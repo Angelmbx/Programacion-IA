52207e9be4456a414205b4574b41ff0af243767d
Angelmbx <mirandabl.angel@gmail.com>
2025-01-24 22:30:39 +0100

    fix(iniciar_partida( )): Solucionado error a la hora de actualizar puntuaciones

--- UD2/Tareas/Tarea_53/src/blackjack.py
+++ UD2/Tareas/Tarea_53/src/blackjack.py
@@ -12,36 +12,27 @@ def seleccionar_participantes():
 
 
 def iniciar_partida():
-    puntuacion_obj = 21 
+    puntuacion_obj = 21
 
     participantes = seleccionar_participantes()
-    jugador1 = participantes[0]
-    jugador2 = participantes[1]
-    banca = participantes[2]
-    
+    jugador1, jugador2, banca = participantes
 
-    estado_inicial= [
-        (jugador1, jugador1.mano.puntuacion),
-        (jugador2, jugador2.mano.puntuacion),
-        (banca, banca.mano.puntuacion)
-    ]
-    
+    estado_inicial = [jugador1, jugador2, banca]
 
     # Comprueba que la puntuación inicial sea menor o igual a 21
-    validos = [jugador for jugador in estado_inicial if jugador[1] <= puntuacion_obj]
-    no_validos = [jugador for jugador in estado_inicial if jugador[1] > puntuacion_obj]
+    validos = [jugador for jugador in estado_inicial if jugador.mano.puntuacion <= puntuacion_obj]
+    no_validos = [jugador for jugador in estado_inicial if jugador.mano.puntuacion > puntuacion_obj]
 
     if validos:
         jugadores_eliminados = []  # Lista para registrar jugadores eliminados
-        
-        for jugador, jugador.mano.puntuacion in validos:
-            # puntuacion = jugador.mano.puntacion
+
+        for jugador in validos:
             print(f"Turno de {jugador.nombre} - Puntuación inicial {jugador.mano.puntuacion}: ")
-    
+
             jugando = True
-            
+
             if jugador.mano.puntuacion == puntuacion_obj:
-                print(f"{jugador.nombre} ha alcanzado la puntuación límite con {jugador.mano.puntuacion} puntos!.")
+                print(f"{jugador.nombre} ha alcanzado la puntuación límite con {jugador.mano.puntuacion} puntos!")
                 jugando = False
 
             while jugando:
@@ -49,38 +40,29 @@ def iniciar_partida():
                 if pedir == "1":
                     jugador.mano.añadir_carta()
                     print(f"Nueva puntuación {jugador.mano.puntuacion}")
-                    
+
                     if jugador.mano.puntuacion == puntuacion_obj:
-                        print(f"{jugador.nombre} ha alcanzado la puntuación límite con {jugador.mano.puntuacion} puntos!.")
+                        print(f"{jugador.nombre} ha alcanzado la puntuación límite con {jugador.mano.puntuacion} puntos!")
                         break
                     elif jugador.mano.puntuacion > puntuacion_obj:
                         print(f"{jugador.nombre} ha superado la puntuación límite con {jugador.mano.puntuacion}.")
-                        jugadores_eliminados.append((jugador, jugador.mano.puntuacion))
+                        jugadores_eliminados.append(jugador)
                         break
                 else:
                     jugando = False
-        
 
-        # Actualizar la lista de válidos eliminando los jugadores que superaron 21 puntos
+        # Actualiza la lista de válidos eliminando los jugadores que superaron 21 puntos
         validos = [jugador for jugador in validos if jugador not in jugadores_eliminados]
-        
-    else:
-        for jugador, puntuacion in no_validos:
-            print(f"{jugador.nombre} superó la puntuación límite: {puntuacion}: ")
 
+    else:
+        for jugador in no_validos:
+            print(f"{jugador.nombre} superó la puntuación límite: {jugador.mano.puntuacion}: ")
 
     return validos
 
 
-
 def comprobar_ganadores():
+    participantes_validos = iniciar_partida()
 
-
-    puntuaciones_finales = iniciar_partida()
-    #print(puntuaciones_finales)
-
-    for elemento in puntuaciones_finales:
-        jugador = elemento[0]
-        puntuacion = elemento[1]
-        print(jugador.nombre, puntuacion)
-        
+    for jugador in participantes_validos:
+        print(f"{jugador.nombre} {jugador.mano.puntuacion}")
