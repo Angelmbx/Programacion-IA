8bb99d0fddb7cdeec19635dcdc17c6a72c795635
Angelmbx <mirandabl.angel@gmail.com>
2025-04-04 19:10:47 +0200

    Tarea 66 corregida para leer bien el fichero

--- UD2/Tareas/Tarea_23.py
+++ UD2/Tareas/Tarea_23.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
             
     if numero_primo:
         print("O número introducido é primo")
-    else:
+    # else:
         print("O número introducido NON é primo")
